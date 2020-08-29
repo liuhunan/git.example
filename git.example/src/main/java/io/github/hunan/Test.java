b6c573c3a10f1e1b5fb5a054185c0f4fb1cd80ad
liuhunan <704769524@qq.com>
2020-08-29 17:45:38 +0800

    dev-100新功能需求提交

--- git.example/src/main/java/io/github/hunan/Test.java
+++ git.example/src/main/java/io/github/hunan/Test.java
@@ -1,7 +1,7 @@
 package io.github.hunan;
 
 public class Test {
-    public static void main(String[] args) {
-
+    public static void main(string[] args) {
+        System.out.println('dev-100');
     }
 }
