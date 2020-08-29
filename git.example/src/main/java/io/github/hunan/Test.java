ea184573761d6882fdd949d72f5418184096dba9
liuhunan <704769524@qq.com>
2020-08-29 21:20:00 +0800

    完善dev-100的功能

--- git.example/src/main/java/io/github/hunan/Test.java
+++ git.example/src/main/java/io/github/hunan/Test.java
@@ -3,5 +3,6 @@ package io.github.hunan;
 public class Test {
     public static void main(string[] args) {
         System.out.println('dev-100的新功能已完成');
+        System.out.println('完善dev-100的新功能');
     }
 }
