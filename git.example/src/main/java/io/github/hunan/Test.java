7be09a00401cf2e60f4ed0274587faea2fa0affc
liuhunan <704769524@qq.com>
2020-08-29 18:10:52 +0800

    dev-100新功能已完成

--- git.example/src/main/java/io/github/hunan/Test.java
+++ git.example/src/main/java/io/github/hunan/Test.java
@@ -2,6 +2,6 @@ package io.github.hunan;
 
 public class Test {
     public static void main(string[] args) {
-        System.out.println('dev-100');
+        System.out.println('dev-100的新功能已完成');
     }
 }
