4d906f772a2bde3c8da1a4816c23fa4fa8f2e313
liuhunan <704769524@qq.com>
2020-08-29 21:18:18 +0800

    合并dev-100

--- git.example/src/main/java/io/github/hunan/Test.java
+++ git.example/src/main/java/io/github/hunan/Test.java
@@ -3,9 +3,8 @@ package io.github.hunan;
 public class Test {
     public static void main(string[] args) {
         System.out.println('dev-100的新功能已完成');
-    public static void main(String[] args) {
-    System.out.print("master分支内容修改");
-    System.out.print("master分支内容1修改");
+        System.out.print("master分支内容修改");
+        System.out.print("master分支内容1修改");
 
     }
 }
